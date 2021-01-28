1abdda7b8ac7b6f659ac071c9a23a6949b1b9c9b
bkc-tomi <bkc.tomi1979@gamil.com>
2021-01-28 15:32:59 +0900

    テストファイル作成

--- web/errors/tests.py
+++ web/errors/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from pyground.tests import CommonTestCase
+from django.urls    import reverse
 
-# Create your tests here.
+
+# モデル
+from bookmark.models   import Bookmark
+from friend.models     import Follow, Permit
+from playground.models import Code
+from question.models   import Question, Correcter
+from user.models       import User, Profile
+
+# templateのあるviewのみテスト
+# POST処理等は取りあえすしない
+
+class IndexViewTest(CommonTestCase):
+    """
+    ----------------------------------------------------------------------
+    エラーページ
+    ----------------------------------------------------------------------
+    """
+    def test_show_errors(self):
+        """
+        ----------------------------------------------------------------
+        ** テスト内容 **
+        作成したエラーを表示する
+
+        ** 入力値と期待値 **
+        errors: [
+            
+        ]
+        ----------------------------------------------------------------
+        """
